5506ac863fc5d048d4d84dbb1f9a57b1690c18e7
harshjalutharia <harshjalutharia@gmail.com>
2019-06-21 23:13:34 +0530

    Completed tank movement. Tank still slides from left to right.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -9,16 +9,6 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	ATank* ControlledTank = GetControlledTank();
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Error, TEXT("No player possessed tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Player possessed tank name: %s"), *(ControlledTank->GetName()));
-	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
